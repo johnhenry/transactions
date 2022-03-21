7f6813159dda3c163436a77689b7da2f006100a6
John <john@iamjohnhenry.com>
2022-03-21 09:20:29 -0700

    update branch urls

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,3 @@
+// export const DEFAULT_URI = "http://localhost:8080/graphql";
 // export const DEFAULT_URI = "https://afraid-spider-68.deno.dev/graphql";
-export const DEFAULT_URI = "http://localhost:8080/graphql";
+export const DEFAULT_URI = "https://afraid-spider-69.deno.dev/graphql";
